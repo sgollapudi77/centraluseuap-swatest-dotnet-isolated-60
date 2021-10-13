87008a577943ed925bd1fe05eca02b7cb621d7d6
Kamesh Tanneru <kamesrao@microsoft.com>
2021-10-13 15:57:53 +0530

    feat: add app, api

---
+++ api/HttpExample.cs
@@ -0,0 +1,30 @@
+namespace dotnet_isolated_60
+{
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    using Microsoft.Azure.Functions.Worker;
+
+    using Microsoft.Azure.Functions.Worker.Http;
+
+    using Microsoft.Extensions.Logging;
+
+
+    public static class HttpExample
+    {
+        [Function("HttpExample")]
+        public static HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            string date = System.DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
+
+            response.WriteString(date + " abcd");
+            return response;
+        }
+    }
+}

41ec24f70f9a6b4b9e909b87bfe2d94c149c3247
Kamesh Tanneru <36271852+tkamesh@users.noreply.github.com>
2021-10-16 20:46:51 +0530

    Update HttpExample.cs

--- api/HttpExample.cs
+++ api/HttpExample.cs
@@ -21,7 +21,7 @@ namespace dotnet_isolated_60
         {
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-string date = "2021-10-16T15:04:55.785Z";
+string date = "2021-10-16T15:04:55.785Z  ";
 
             response.WriteString(date);
             return response;
